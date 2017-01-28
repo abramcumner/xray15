b31060628cf7304a33149a1d2fd1c74e342cb3cb
abramcumner <abramcumner@yandex.ru>
2017-01-28 16:35:56 +0300

    [SDK] теперь в компиляторах выводится корректная дата сборки и номер билда xrCore

--- xray/utils/xrDO_Light/xrDO_Light.cpp
+++ xray/utils/xrDO_Light/xrDO_Light.cpp
@@ -19,6 +19,50 @@ static const char* h_str =
 void Help()
 {	MessageBox(0,h_str,"Command line options",MB_OK|MB_ICONINFORMATION); }
 
+// computing build id
+XRCORE_API	LPCSTR	build_date;
+XRCORE_API	u32		build_id;
+static LPSTR month_id[12] = {
+	"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
+};
+
+static int days_in_month[12] = {
+	31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
+static int start_day = 31;	// 31
+static int start_month = 1;	// January
+static int start_year = 1999;	// 1999
+
+void compute_build_id()
+{
+	build_date = __DATE__;
+
+	int					days;
+	int					months = 0;
+	int					years;
+	string16			month;
+	string256			buffer;
+	strcpy_s(buffer, __DATE__);
+	sscanf(buffer, "%s %d %d", month, &days, &years);
+
+	for (int i = 0; i<12; i++) {
+		if (_stricmp(month_id[i], month))
+			continue;
+
+		months = i;
+		break;
+	}
+
+	build_id = (years - start_year) * 365 + days - start_day;
+
+	for (int i = 0; i<months; ++i)
+		build_id += days_in_month[i];
+
+	for (int i = 0; i<start_month - 1; ++i)
+		build_id -= days_in_month[i];
+}
+
 void Startup(LPSTR     lpCmdLine)
 {
 	char cmd[512],name[256];
@@ -68,6 +112,7 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 {
 	// Initialize debugging
 	Debug._initialize	(false);
+	compute_build_id();
 	Core._initialize	("xrDO");
 	Startup				(lpCmdLine);
 	
