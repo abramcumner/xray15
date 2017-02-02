edba8d6d892555dc870135476fe3d6aac9ffb7f2
abramcumner <abramcumner@yandex.ru>
2017-02-02 20:42:07 +0300

    теперь при отсутствии звукого файла пишется сообщение в лог и используется звук sounds\$no_sound.ogg

--- xray/xrSound/SoundRender_Source_loader.cpp
+++ xray/xrSound/SoundRender_Source_loader.cpp
@@ -138,11 +138,11 @@ void CSoundRender_Source::load(LPCSTR name)
 	strconcat			(sizeof(fn),fn,N,".ogg");
 	if (!FS.exist("$level$",fn))	FS.update_path	(fn,"$game_sounds$",fn);
 
-#ifdef _EDITOR
-	if (!FS.exist(fn)){ 
+	if (!FS.exist(fn)) {
+		Msg("! Can't find sound '%s'", name);
 		FS.update_path	(fn,"$game_sounds$","$no_sound.ogg");
     }
-#endif
+
 	LoadWave			(fn);
 	SoundRender->cache.cat_create	(CAT, dwBytesTotal);
 }
