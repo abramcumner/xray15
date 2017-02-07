6702458a0df5a6f384caf6c9daad1bf3c79b92f5
abramcumner <abramcumner@yandex.ru>
2017-02-08 00:14:32 +0300

    звуки торговца теперь идут от торговца, а не проигрываются в голове у ГГ

--- xray/xrGame/ai/trader/trader_animation.cpp
+++ xray/xrGame/ai/trader/trader_animation.cpp
@@ -67,8 +67,8 @@ void CTraderAnimation::set_sound(LPCSTR sound, LPCSTR anim)
 	set_head_animation	(anim);
 
 	m_sound				= xr_new<ref_sound>();
-	m_sound->create		(sound,st_Effect,SOUND_TYPE_WORLD);
-	m_sound->play		(NULL, sm_2D);
+	m_sound->create		(sound, st_Effect, SOUND_TYPE_WORLD);
+	m_sound->play		(m_trader);
 }
 
 void CTraderAnimation::remove_sound()
@@ -115,7 +115,7 @@ void CTraderAnimation::external_sound_start(LPCSTR phrase)
 	
 	m_sound					= xr_new<ref_sound>();
 	m_sound->create			(phrase,st_Effect,SOUND_TYPE_WORLD);
-	m_sound->play			(NULL, sm_2D);
+	m_sound->play			(m_trader);
 
 	m_motion_head.invalidate();
 }
