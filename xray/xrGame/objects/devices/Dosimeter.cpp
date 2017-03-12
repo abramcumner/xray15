0cb7d42b4bed47e5d9350f95e92898d786ae3140
abramcumner <abramcumner@yandex.ru>
2017-03-12 15:55:42 +0300

    класс СUIDosimeter вынесен в отдельные файлы

--- xray/xrGame/objects/devices/Dosimeter.cpp
+++ xray/xrGame/objects/devices/Dosimeter.cpp
@@ -1,151 +1,51 @@
 #include "stdafx.h"
 #include "Dosimeter.h"
-#include "../../player_hud.h"
-#include "../../../Include/xrRender/UIRender.h"
-#include "../../ui/UIXmlInit.h"
-#include "../../ui/xrUIXmlParser.h"
-#include "../../ui/UIStatic.h"
-
-#include "../../ui/UIHudStatesWnd.h"
-#include "../../Level.h"
-#include "../../HUDManager.h"
-#include "../../ui/UIMainIngameWnd.h"
+#include "UIDosimeter.h"
 
 void CDosimeter::Load(LPCSTR section)
 {
-	CHudItemObject::Load(section);
+    CHudItemObject::Load(section);
 
-	m_sounds.LoadSound(section, "snd_draw", "sndShow");
-	m_sounds.LoadSound(section, "snd_holster", "sndHide");
+    m_sounds.LoadSound(section, "snd_draw", "sndShow");
+    m_sounds.LoadSound(section, "snd_holster", "sndHide");
 }
 
 void CDosimeter::shedule_Update(u32 dt)
 {
-	CHudItemObject::shedule_Update(dt);
+    CHudItemObject::shedule_Update(dt);
 
-	if (!IsWorking())			return;
+    if (!IsWorking())
+        return;
 
-	Position().set(H_Parent()->Position());
+    Position().set(H_Parent()->Position());
 }
 
 void CDosimeter::UpdateAf()
 {
-
 }
 
 void CDosimeter::CreateUI()
 {
-	R_ASSERT(NULL == m_ui);
-	m_ui = xr_new<CUIDosimeter>();
-	ui().construct(this);
+    R_ASSERT(NULL == m_ui);
+    m_ui = xr_new<CUIDosimeter>();
+    ui().construct(this);
 }
 
-CUIDosimeter&  CDosimeter::ui()
+CUIDosimeter& CDosimeter::ui()
 {
-	return *((CUIDosimeter*)m_ui);
+    return *((CUIDosimeter*)m_ui);
 }
 
-bool  CDosimeter::render_item_3d_ui_query()
+bool CDosimeter::render_item_3d_ui_query()
 {
-	return IsWorking();
+    return IsWorking();
 }
 
 void CDosimeter::render_item_3d_ui()
 {
-	R_ASSERT(HudItemData());
-	CCustomDetector::render_item_3d_ui();
-	ui().Draw();
-	//	Restore cull mode
-	UIRender->CacheSetCullMode(IUIRender::cmCCW);
-}
-
-void CUIDosimeter::construct(CDosimeter* p)
-{
-	m_parent = p;
-	CUIXml								uiXml;
-	uiXml.Load(CONFIG_PATH, UI_PATH, "ui_dosimeter.xml");
-
-	CUIXmlInit							xml_init;
-
-	xml_init.InitWindow(uiXml, "dosimeter", 0, this);
-
-	m_wrk_area = xr_new<CUIStatic>();
-	xml_init.InitStatic(uiXml, "dosimeter:wrk_area", 0, m_wrk_area);
-	m_wrk_area->SetAutoDelete(true);
-	AttachChild(m_wrk_area);
-
-	m_seg1 = xr_new<CUIStatic>();
-	xml_init.InitStatic(uiXml, "dosimeter:seg1", 0, m_seg1);
-	m_seg1->SetAutoDelete(true);
-	AttachChild(m_seg1);
-
-	m_seg2 = xr_new<CUIStatic>();
-	xml_init.InitStatic(uiXml, "dosimeter:seg2", 0, m_seg2);
-	m_seg2->SetAutoDelete(true);
-	AttachChild(m_seg2);
-
-	m_seg3 = xr_new<CUIStatic>();
-	xml_init.InitStatic(uiXml, "dosimeter:seg3", 0, m_seg3);
-	m_seg3->SetAutoDelete(true);
-	AttachChild(m_seg3);
-
-	m_seg4 = xr_new<CUIStatic>();
-	xml_init.InitStatic(uiXml, "dosimeter:seg4", 0, m_seg4);
-	m_seg4->SetAutoDelete(true);
-	AttachChild(m_seg4);
-
-	Fvector _map_attach_p = pSettings->r_fvector3(m_parent->cNameSect(), "ui_p");
-	Fvector _map_attach_r = pSettings->r_fvector3(m_parent->cNameSect(), "ui_r");
-
-	_map_attach_r.mul(PI / 180.f);
-	m_map_attach_offset.setHPB(_map_attach_r.x, _map_attach_r.y, _map_attach_r.z);
-	m_map_attach_offset.translate_over(_map_attach_p);
-}
-
-void CUIDosimeter::update()
-{
-	CUIArtefactDetectorBase::update();
-
-	float rad = HUD().GetUI()->UIMainIngameWnd->get_hud_states()->get_main_sensor_value();
-	rad *= 1000;
-	if (rad > 9999.0f) rad = 9999.0f;
-	string16 s;
-	sprintf_s(s, "%05.0lf", rad);
-	string16 tex;
-	sprintf_s(tex, "green_%c", s[1]);
-	m_seg1->InitTextureEx(tex, "hud\\p3d");
-	sprintf_s(tex, "green_%c", s[2]);
-	m_seg2->InitTextureEx(tex, "hud\\p3d");
-	sprintf_s(tex, "green_%c", s[3]);
-	m_seg3->InitTextureEx(tex, "hud\\p3d");
-	sprintf_s(tex, "green_%c", s[4]);
-	m_seg4->InitTextureEx(tex, "hud\\p3d");
-
-	CUIWindow::Update();
-}
-
-void CUIDosimeter::Draw()
-{
-	Fmatrix						LM;
-	GetUILocatorMatrix(LM);
-
-	IUIRender::ePointType bk = UI()->m_currentPointType;
-
-	UI()->m_currentPointType = IUIRender::pttLIT;
-
-	UIRender->CacheSetXformWorld(LM);
-	UIRender->CacheSetCullMode(IUIRender::cmNONE);
-
-	CUIWindow::Draw();
-
-	UI()->m_currentPointType = bk;
-}
-
-void CUIDosimeter::GetUILocatorMatrix(Fmatrix& _m)
-{
-	Fmatrix	trans = m_parent->HudItemData()->m_item_transform;
-	u16 bid = m_parent->HudItemData()->m_model->LL_BoneID("substrate_joint");
-	Fmatrix cover_bone = m_parent->HudItemData()->m_model->LL_GetTransform(bid);
-	_m.mul(trans, cover_bone);
-	_m.mulB_43(m_map_attach_offset);
+    R_ASSERT(HudItemData());
+    CCustomDetector::render_item_3d_ui();
+    ui().Draw();
+    //	Restore cull mode
+    UIRender->CacheSetCullMode(IUIRender::cmCCW);
 }
--- xray/xrGame/objects/devices/Dosimeter.h
+++ xray/xrGame/objects/devices/Dosimeter.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "../../CustomDetector.h"
 
 class CUIDosimeter;
@@ -6,34 +7,14 @@ class CUIDosimeter;
 class CDosimeter : public CCustomDetector
 {
 public:
-	void Load(LPCSTR section) override;
-	void shedule_Update(u32 dt) override;
+    void Load(LPCSTR section) override;
+    void shedule_Update(u32 dt) override;
 
-	void render_item_3d_ui() override;
-	bool render_item_3d_ui_query() override;
+    void render_item_3d_ui() override;
+    bool render_item_3d_ui_query() override;
 
 protected:
-	void UpdateAf() override;
-	void CreateUI() override;
-	CUIDosimeter& ui();
-};
-
-class CUIDosimeter : public CUIArtefactDetectorBase, public CUIWindow
-{
-public:
-	void update() override;
-	void Draw() override;
-
-	void construct(CDosimeter* p);
-
-private:
-	CUIStatic* m_wrk_area;
-	CUIStatic* m_seg1;
-	CUIStatic* m_seg2;
-	CUIStatic* m_seg3;
-	CUIStatic* m_seg4;
-	CDosimeter* m_parent;
-	Fmatrix m_map_attach_offset;
-
-	void GetUILocatorMatrix(Fmatrix& _m);
+    void UpdateAf() override;
+    void CreateUI() override;
+    CUIDosimeter& ui();
 };
