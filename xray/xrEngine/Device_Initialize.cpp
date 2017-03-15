cc949941c15bd4fa62e76bfa2a4acca266f4882f
abramcumner <abramcumner@yandex.ru>
2017-03-15 03:22:51 +0300

    - добавлены отладочные сообщения при загрузке длл редактора - длл редактора собираются теперь под XP

--- xray/xrEngine/Device_Initialize.cpp
+++ xray/xrEngine/Device_Initialize.cpp
@@ -12,24 +12,27 @@ extern LRESULT CALLBACK WndProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 #ifdef INGAME_EDITOR
 void CRenderDevice::initialize_editor	()
 {
+    Msg("loading \"editor.dll\"...");
 	m_editor_module		= LoadLibrary("editor.dll");
 	if (!m_editor_module) {
 		Msg				("! cannot load library \"editor.dll\"");
 		return;
 	}
+    Msg("\"editor.dll\" loaded successfully");
 
 	m_editor_initialize	= (initialize_function_ptr)GetProcAddress(m_editor_module, "initialize");
-	VERIFY				(m_editor_initialize);
+	R_ASSERT			(m_editor_initialize);
 
 	m_editor_finalize	= (finalize_function_ptr)GetProcAddress(m_editor_module, "finalize");
-	VERIFY				(m_editor_finalize);
+    R_ASSERT(m_editor_finalize);
 
 	m_engine			= xr_new<engine_impl>();
 	m_editor_initialize	(m_editor, m_engine);
-	VERIFY				(m_editor);
+    R_ASSERT(m_editor);
 
 	m_hWnd				= m_editor->view_handle();
-	VERIFY				(m_hWnd != INVALID_HANDLE_VALUE);
+    R_ASSERT(m_hWnd != INVALID_HANDLE_VALUE);
+    Msg("editor initialized");
 }
 #endif // #ifdef INGAME_EDITOR
 
