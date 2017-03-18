20f7cd45f60ffb83d9ad6c52277d71b56e285097
abramcumner <abramcumner@yandex.ru>
2017-03-19 01:01:45 +0300

    вынесен Precompiled header в отдельную папку

--- xray/Layers/xrRenderPC_R2/pch/stdafx.h
+++ xray/Layers/xrRenderPC_R2/pch/stdafx.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #pragma warning(disable:4995)
-#include "../../xrEngine/stdafx.h"
+#include "../../../xrEngine/stdafx.h"
 #pragma warning(disable:4995)
 #include <d3dx9.h>
 #pragma warning(default:4995)
@@ -14,29 +14,29 @@
 #pragma warning( 4 : 4244 )
 #pragma warning(disable:4237)
 
-#include "../xrRender/xrD3DDefs.h"
+#include "../../xrRender/xrD3DDefs.h"
 
-#include "../xrRender/HW.h"
-#include "../xrRender/Shader.h"
-#include "../xrRender/R_Backend.h"
-#include "../xrRender/R_Backend_Runtime.h"
+#include "../../xrRender/HW.h"
+#include "../../xrRender/Shader.h"
+#include "../../xrRender/R_Backend.h"
+#include "../../xrRender/R_Backend_Runtime.h"
 
 #define		R_R1	1
 #define		R_R2	2
 #define		R_R3	3
 #define		RENDER	R_R2
 
-#include "../xrRender/resourcemanager.h"
+#include "../../xrRender/resourcemanager.h"
 
-#include "../../xrEngine/vis_common.h"
-#include "../../xrEngine/render.h"
-#include "../../xrEngine/_d3d_extensions.h"
-#include "../../xrEngine/igame_level.h"
-#include "../xrRender/blenders\blender.h"
-#include "../xrRender/blenders\blender_clsid.h"
-#include "../../xrEngine/psystem.h"
-#include "../xrRender/xrRender_console.h"
-#include "r2.h"
+#include "../../../xrEngine/vis_common.h"
+#include "../../../xrEngine/render.h"
+#include "../../../xrEngine/_d3d_extensions.h"
+#include "../../../xrEngine/igame_level.h"
+#include "../../xrRender/blenders\blender.h"
+#include "../../xrRender/blenders\blender_clsid.h"
+#include "../../../xrEngine/psystem.h"
+#include "../../xrRender/xrRender_console.h"
+#include "../r2.h"
 
 IC	void	jitter(CBlender_Compile& C)
 {
