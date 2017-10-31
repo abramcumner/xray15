d1c663c0a8c9addb43bd02d9b832b8c2149ea016
abramcumner <abramcumner@yandex.ru>
2017-10-31 14:07:23 +0300

    исправлен вылет с нехваткой памяти при большом числе потоков.
    В каждом потоке создается кеш xr_vector<RC>, и если потоков много, то заканчивается память на x86.

--- xray/utils/xrAI/compiler_cover.cpp
+++ xray/utils/xrAI/compiler_cover.cpp
@@ -231,7 +231,7 @@ struct	RC { RayCache	C; };
 class	CoverThread : public CThread
 {
 	u32					Nstart, Nend;
-	xr_vector<RC>		cache;
+	xr_hash_map<u32, RC> cache;
 	CDB::COLLIDER		DB;
 	Query				Q;
 
@@ -299,14 +299,6 @@ public:
 	virtual void		Execute()
 	{
 		DB.ray_options		(CDB::OPT_CULL);
-		{
-			RC				rc;	
-			rc.C[0].set		(0,0,0); 
-			rc.C[1].set		(0,0,0); 
-			rc.C[2].set		(0,0,0);
-			
-			cache.assign	(g_nodes.size()*2,rc);
-		}
 
 		FPU::m24r		();
 
