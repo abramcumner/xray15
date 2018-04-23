fe35f1bb0d2ef071762b82e1be9a682322e23345
abramcumner <abramcumner@yandex.ru>
2018-04-23 20:54:05 +0300

    исправлен импорт функций из xrSE_Factory.dll.

--- xray/utils/xrAI/xrAI.cpp
+++ xray/utils/xrAI/xrAI.cpp
@@ -237,8 +237,20 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	if (0==hFactory)		R_CHK			(GetLastError());
 	R_ASSERT2				(hFactory,"Factory DLL raised exception during loading or there is no factory DLL at all");
 
-	create_entity			= (Factory_Create*)		GetProcAddress(hFactory,"_create_entity@4");	R_ASSERT(create_entity);
-	destroy_entity			= (Factory_Destroy*)	GetProcAddress(hFactory,"_destroy_entity@4");	R_ASSERT(destroy_entity);
+	create_entity			= (Factory_Create*)		GetProcAddress(hFactory,
+#ifdef _WIN64
+		"create_entity"
+#else
+		"_create_entity@4"
+#endif
+	);	R_ASSERT(create_entity);
+	destroy_entity			= (Factory_Destroy*)	GetProcAddress(hFactory,
+#ifdef _WIN64
+		"destroy_entity"
+#else
+		"_destroy_entity@4"
+#endif
+	);	R_ASSERT(destroy_entity);
 
 	Msg("Command line: '%s'\n", lpCmdLine);
 	Startup					(lpCmdLine);
